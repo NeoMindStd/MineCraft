e5c350e98d2b96d654c33e45eee9c7ce6fa93a0a
NeoMindStd <dwj1996@naver.com>
2021-12-03 18:03:36 +0000

    backup 2112040302

---
+++ start.bat
@@ -0,0 +1,3 @@
+@echo off
+java -Xmx7G -Xms2G -jar ../spigot-1.18.jar
+pause
---
+++ start.sh
@@ -0,0 +1,2 @@
+screen -S mcs java -Xmx6G -Xms2G -jar ../spigot-1.18.jar nogui
+#java -Xmx6G -Xms2G -jar ../spigot-1.18.jar nogui
